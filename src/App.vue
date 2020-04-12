33177c3bf22df756447677b4d8befce655a61448
sokaniwa <sokaniwa@itrealize.co.jp>
2020-04-12 20:50:08 +0900

    install opencv

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <OpenCvTest/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import OpenCvTest from './components/OpenCvTest.vue'
 
 @Component({
   components: {
+    OpenCvTest
+  }
 })
 export default class App extends Vue {}
 </script>
---
+++ src/components/OpenCvTest.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+const cv = require('opencv.js')
+
+@Component
+export default class OpenCvTest extends Vue {}
+</script>
